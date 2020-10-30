c5abe8968076330f96d29325a0ee99ea3d608442
Tom Keddie <git@bronwenandtom.com>
2020-10-30 16:28:00 -0700

    software: change to 5 channels, sweep full 8 bit range

--- software/sao-wall.c
+++ software/sao-wall.c
@@ -4,8 +4,7 @@
 
 // Pins
 // A_PWM   OC0A PB7 (Arduino D11)
-// B_GPIO1 OC3A PC6 (Arduino D5)
-// B_GPIO2 OC4A PC7 (Arduino D13)
+// B_PWM   OC3A PC6 (Arduino D5)
 // C_PWM   OC1A PB5 (Arduino D9)
 // D_GPIO2 OC1B PB6 (Arduino D10)
 // D_GPIO1 OC4D PD7 (Arduino D6)
@@ -15,43 +14,24 @@
 #define C_GPIO1 PF4
 #define C_GPIO2 PF5
 
-static volatile uint8_t m_level0 = 0;
-static volatile uint8_t m_level1 = 2;
-static volatile uint8_t m_level2 = 5;
-static volatile uint8_t m_level3 = 8;
-static volatile uint8_t m_level4 = 11;
-static volatile uint8_t m_level5 = 14;
+static volatile uint8_t m_level0 = 4+0*256/5;
+static volatile uint8_t m_level1 = 4+1*256/5;
+static volatile uint8_t m_level2 = 4+2*256/5;
+static volatile uint8_t m_level3 = 4+3*256/5;
+static volatile uint8_t m_level4 = 4+4*256/5;
 
-static const uint8_t m_level_to_count[] = {15, 1*16+15, 2*16+15, 3*16+15, 4*16+15, 5*16+15, 6*16+15, 7*16+15, 8*16+15, 9*16+15, 10*16+15, 11*16+15, 12*16+15, 13*16+15, 14*16+15, 15*16+15};
 
 static void update_ocr(void) {
-    OCR0A = m_level_to_count[m_level0];
-    OCR3A = m_level_to_count[m_level1];
-    OCR4A = m_level_to_count[m_level2];
-    OCR1A = m_level_to_count[m_level3];
-    OCR1B = m_level_to_count[m_level4];
-    OCR4D = m_level_to_count[m_level5];
+    OCR0A = m_level0;
+    OCR3A = m_level1;
+    OCR1A = m_level2;
+    OCR1B = m_level3;
+    OCR4D = m_level4;
 }    
 
 void main(void) {
-#if 0     
-    // Power Reduction Register 0
-    PRR0    =   (0<<PRTWI)  |   // Bit 7 - PRTWI: TWI
-                (0<<PRTIM2) |   // Bit 6 - Res: Reserved bit
-                (0<<PRTIM0) |   // Bit 5 - PRTIM0: Timer/Counter0
-                (0<<PRTIM1) |   // Bit 3 - PRTIM1: Timer/Counter1
-                (0<<PRSPI)  |   // Bit 2 - PRSPI: SPI
-                (0<<PRADC);     // Bit 0 - PRADC: PADC
-
-    // Power Reduction Register 1
-    PRR1    =   (0<<PRUSB)  |   // Bit 7 - PRUSB: USB
-                (0<<PRTIM4) |   // Bit 4- PRTIM4: Timer/Counter4
-                (0<<PRTIM3) |   // Bit 3 - PRTIM3: Timer/Counter3
-                (0<<PRUSART1);  // Bit 0 - PRUSART1: USART1
-#endif    
-    
     DDRB = (1 << DDB5) | (1 << DDB6) | (1 << DDB7);
-    DDRC = (1 << DDC6) | (1 << DDC7);
+    DDRC = (1 << DDC6);
     DDRD = (1 << DDD7) | (1 << DDD1);
     DDRF = (1 << DDF5) | (1 << DDF4) | (1 << DDF1) | (1 << DDF0);
 
@@ -90,21 +70,18 @@ ISR(TIMER0_OVF_vect) {
     static uint8_t increment2 = 1;
     static uint8_t increment3 = 1;
     static uint8_t increment4 = 1;
-    static uint8_t increment5 = 1;
-    if (++counter == 122) {
+    if (++counter == 8) {
         counter = 0;
         if (increment0) m_level0++; else m_level0--;
         if (increment1) m_level1++; else m_level1--;
         if (increment2) m_level2++; else m_level2--;
         if (increment3) m_level3++; else m_level3--;
         if (increment4) m_level4++; else m_level4--;
-        if (increment5) m_level5++; else m_level5--;
-        if (m_level0 == 15) increment0 = 0; if (m_level0 == 0) increment0 = 1;
-        if (m_level1 == 15) increment1 = 0; if (m_level1 == 0) increment1 = 1;
-        if (m_level2 == 15) increment2 = 0; if (m_level2 == 0) increment2 = 1;
-        if (m_level3 == 15) increment3 = 0; if (m_level3 == 0) increment3 = 1;
-        if (m_level4 == 15) increment4 = 0; if (m_level4 == 0) increment4 = 1;
-        if (m_level5 == 15) increment5 = 0; if (m_level5 == 0) increment5 = 1;
+        if (m_level0 == 0xff) increment0 = 0; if (m_level0 == 4) increment0 = 1;
+        if (m_level1 == 0xff) increment1 = 0; if (m_level1 == 4) increment1 = 1;
+        if (m_level2 == 0xff) increment2 = 0; if (m_level2 == 4) increment2 = 1;
+        if (m_level3 == 0xff) increment3 = 0; if (m_level3 == 4) increment3 = 1;
+        if (m_level4 == 0xff) increment4 = 0; if (m_level4 == 4) increment4 = 1;
         update_ocr();
     }
 }
